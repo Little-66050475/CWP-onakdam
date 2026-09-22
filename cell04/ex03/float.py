e7cb66161c183ac0cc950e74729da3ffe0378766
Oangkharit <66050475@kmitl.ac.th>
2026-09-22 16:01:39 +0700

    Create float.py

---
+++ cell04/ex03/float.py
@@ -0,0 +1,10 @@
+try:
+    user_input = input("Give me a number: ")
+    number = float(user_input)
+    if number.is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+
+except ValueError:
+    pass
