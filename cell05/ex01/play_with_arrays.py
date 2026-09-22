1ba76f47da625364b632e24536a5d508cde21040
Oangkharit <66050475@kmitl.ac.th>
2026-09-22 16:44:09 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-original_array = [2, 4, 6, 67, 7, 99, -13, 1]
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = [x + 2 for x in original_array]
 
 print(f"Original array: {original_array}")
