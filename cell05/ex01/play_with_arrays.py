163c64dfa8eb8c2c2239e15759c1e848415d50d5
Oangkharit <66050475@kmitl.ac.th>
2026-09-22 16:41:44 +0700

    Create play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+original_array = [2, 4, 6, 67, 7, 99, -13, 1]
+new_array = [x + 2 for x in original_array]
+
+print(f"Original array: {original_array}")
+print(f"New array: {new_array}")
